03be263eca6675c819e2884a1eba5f583258f544
megustador <cristiposeletchi@gmail.com>
2017-05-29 15:39:49 +0300

    uploading the stuff

---
+++ Lab-5/WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class primulForm
+    {
+        /// <summary>
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mbFileNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.mbFileOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.mbFileSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.mbFileExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mbEditCut = new System.Windows.Forms.ToolStripMenuItem();
+            this.mbEditCopy = new System.Windows.Forms.ToolStripMenuItem();
+            this.mbEditPaste = new System.Windows.Forms.ToolStripMenuItem();
+            this.mbEditUndo = new System.Windows.Forms.ToolStripMenuItem();
+            this.mbEditSelectAll = new System.Windows.Forms.ToolStripMenuItem();
+            this.mbEditClear = new System.Windows.Forms.ToolStripMenuItem();
+            this.formatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mbFormatFont = new System.Windows.Forms.ToolStripMenuItem();
+            this.mbFormatColor = new System.Windows.Forms.ToolStripMenuItem();
+            this.mbFormatBack = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mbHelpAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.tbText = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.formatToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(697, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mbFileNew,
+            this.mbFileOpen,
+            this.mbFileSave,
+            this.mbFileExit});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
+            // 
+            // mbFileNew
+            // 
+            this.mbFileNew.Name = "mbFileNew";
+            this.mbFileNew.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.mbFileNew.Size = new System.Drawing.Size(152, 22);
+            this.mbFileNew.Text = "New";
+            this.mbFileNew.Click += new System.EventHandler(this.mbFileNew_Click);
+            // 
+            // mbFileOpen
+            // 
+            this.mbFileOpen.Name = "mbFileOpen";
+            this.mbFileOpen.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.mbFileOpen.Size = new System.Drawing.Size(152, 22);
+            this.mbFileOpen.Text = "Open";
+            this.mbFileOpen.Click += new System.EventHandler(this.mbFileOpen_Click);
+            // 
+            // mbFileSave
+            // 
+            this.mbFileSave.Name = "mbFileSave";
+            this.mbFileSave.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.mbFileSave.Size = new System.Drawing.Size(152, 22);
+            this.mbFileSave.Text = "Save";
+            this.mbFileSave.Click += new System.EventHandler(this.mbFileSave_Click);
+            // 
+            // mbFileExit
+            // 
+            this.mbFileExit.Name = "mbFileExit";
+            this.mbFileExit.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.E)));
+            this.mbFileExit.Size = new System.Drawing.Size(152, 22);
+            this.mbFileExit.Text = "Exit";
+            this.mbFileExit.Click += new System.EventHandler(this.mbFileExit_Click_1);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mbEditCut,
+            this.mbEditCopy,
+            this.mbEditPaste,
+            this.mbEditUndo,
+            this.mbEditSelectAll,
+            this.mbEditClear});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // mbEditCut
+            // 
+            this.mbEditCut.Name = "mbEditCut";
+            this.mbEditCut.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.X)));
+            this.mbEditCut.Size = new System.Drawing.Size(174, 22);
+            this.mbEditCut.Text = "Cut";
+            this.mbEditCut.Click += new System.EventHandler(this.mbEditCut_Click);
+            // 
+            // mbEditCopy
+            // 
+            this.mbEditCopy.Name = "mbEditCopy";
+            this.mbEditCopy.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
+            this.mbEditCopy.Size = new System.Drawing.Size(174, 22);
+            this.mbEditCopy.Text = "Copy";
+            this.mbEditCopy.Click += new System.EventHandler(this.mbEditCopy_Click);
+            // 
+            // mbEditPaste
+            // 
+            this.mbEditPaste.Name = "mbEditPaste";
+            this.mbEditPaste.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.V)));
+            this.mbEditPaste.Size = new System.Drawing.Size(174, 22);
+            this.mbEditPaste.Text = "Paste";
+            this.mbEditPaste.Click += new System.EventHandler(this.mbEditPaste_Click);
+            // 
+            // mbEditUndo
+            // 
+            this.mbEditUndo.Name = "mbEditUndo";
+            this.mbEditUndo.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Z)));
+            this.mbEditUndo.Size = new System.Drawing.Size(174, 22);
+            this.mbEditUndo.Text = "Undo";
+            this.mbEditUndo.Click += new System.EventHandler(this.mbEditUndo_Click);
+            // 
+            // mbEditSelectAll
+            // 
+            this.mbEditSelectAll.Name = "mbEditSelectAll";
+            this.mbEditSelectAll.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.A)));
+            this.mbEditSelectAll.Size = new System.Drawing.Size(174, 22);
+            this.mbEditSelectAll.Text = "Select All";
+            this.mbEditSelectAll.Click += new System.EventHandler(this.mbEditSelectAll_Click);
+            // 
+            // mbEditClear
+            // 
+            this.mbEditClear.Name = "mbEditClear";
+            this.mbEditClear.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Shift | System.Windows.Forms.Keys.Delete)));
+            this.mbEditClear.Size = new System.Drawing.Size(174, 22);
+            this.mbEditClear.Text = "Clear All";
+            this.mbEditClear.Click += new System.EventHandler(this.mbEditClear_Click);
+            // 
+            // formatToolStripMenuItem
+            // 
+            this.formatToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mbFormatFont,
+            this.mbFormatColor,
+            this.mbFormatBack});
+            this.formatToolStripMenuItem.Name = "formatToolStripMenuItem";
+            this.formatToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.formatToolStripMenuItem.Text = "Format";
+            // 
+            // mbFormatFont
+            // 
+            this.mbFormatFont.Name = "mbFormatFont";
+            this.mbFormatFont.Size = new System.Drawing.Size(170, 22);
+            this.mbFormatFont.Text = "Font";
+            this.mbFormatFont.Click += new System.EventHandler(this.mbFormatFont_Click);
+            // 
+            // mbFormatColor
+            // 
+            this.mbFormatColor.Name = "mbFormatColor";
+            this.mbFormatColor.Size = new System.Drawing.Size(170, 22);
+            this.mbFormatColor.Text = "Text Color";
+            this.mbFormatColor.Click += new System.EventHandler(this.mbFormatColor_Click);
+            // 
+            // mbFormatBack
+            // 
+            this.mbFormatBack.Name = "mbFormatBack";
+            this.mbFormatBack.Size = new System.Drawing.Size(170, 22);
+            this.mbFormatBack.Text = "Background Color";
+            this.mbFormatBack.Click += new System.EventHandler(this.mbFormatBack_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mbHelpAbout});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // mbHelpAbout
+            // 
+            this.mbHelpAbout.Name = "mbHelpAbout";
+            this.mbHelpAbout.Size = new System.Drawing.Size(107, 22);
+            this.mbHelpAbout.Text = "About";
+            this.mbHelpAbout.Click += new System.EventHandler(this.mbHelpAbout_Click);
+            // 
+            // tbText
+            // 
+            this.tbText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbText.Location = new System.Drawing.Point(0, 24);
+            this.tbText.Multiline = true;
+            this.tbText.Name = "tbText";
+            this.tbText.Size = new System.Drawing.Size(697, 263);
+            this.tbText.TabIndex = 1;
+            this.tbText.TextChanged += new System.EventHandler(this.tbText_TextChanged);
+            // 
+            // primulForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(697, 287);
+            this.Controls.Add(this.tbText);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "primulForm";
+            this.Text = "Aplicatie Notepad";
+            this.Load += new System.EventHandler(this.primulForm_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mbFileNew;
+        private System.Windows.Forms.ToolStripMenuItem mbFileOpen;
+        private System.Windows.Forms.ToolStripMenuItem mbFileSave;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem formatToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.TextBox tbText;
+        private System.Windows.Forms.ToolStripMenuItem mbEditCut;
+        private System.Windows.Forms.ToolStripMenuItem mbEditCopy;
+        private System.Windows.Forms.ToolStripMenuItem mbEditPaste;
+        private System.Windows.Forms.ToolStripMenuItem mbEditUndo;
+        private System.Windows.Forms.ToolStripMenuItem mbEditSelectAll;
+        private System.Windows.Forms.ToolStripMenuItem mbFormatFont;
+        private System.Windows.Forms.ToolStripMenuItem mbFormatColor;
+        private System.Windows.Forms.ToolStripMenuItem mbHelpAbout;
+        private System.Windows.Forms.ToolStripMenuItem mbFormatBack;
+        private System.Windows.Forms.ToolStripMenuItem mbFileExit;
+        private System.Windows.Forms.ToolStripMenuItem mbEditClear;
+
+
+
+    }
+}
+
---
+++ Lab-5/WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication3
+{
+    public partial class primulForm : Form
+    {
+        public primulForm()
+        {
+            InitializeComponent();
+        }
+
+        private void primulForm_Load(object sender, EventArgs e)
+        {
+            Text = "Aplicatie Notepad";
+        }
+
+        private void mbFileNew_Click(object sender, EventArgs e)
+        {
+            if (tbText.Text.Length > 0)
+            {
+                DialogResult rezultatIntrebare = MessageBox.Show("Vrei sa salvezi continutul existent?", "Intrebare", MessageBoxButtons.YesNo);
+                if (rezultatIntrebare == System.Windows.Forms.DialogResult.Yes)
+                {
+                    IntreabaCaleFisiersiSalveaza();
+                }
+                tbText.Text = "";
+            }
+        }
+
+        private void mbFileOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            DialogResult rezultatDialog = dialog.ShowDialog();
+            if(rezultatDialog == System.Windows.Forms.DialogResult.OK)
+            {
+                string continutFisier = File.ReadAllText(dialog.FileName);
+                tbText.Text = continutFisier;
+            }
+        }
+
+        private void mbFileSave_Click(object sender, EventArgs e)
+        {
+            IntreabaCaleFisiersiSalveaza();
+        }
+
+        private void IntreabaCaleFisiersiSalveaza()
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            DialogResult rezultatDialog = dialog.ShowDialog();
+            if (rezultatDialog == System.Windows.Forms.DialogResult.OK)
+            {
+                string numeFisier = dialog.FileName;
+                File.WriteAllText(numeFisier, tbText.Text);
+                MessageBox.Show("Fisierul a fost salvat cu succes");
+            }
+        }
+
+        private void mbEditCut_Click(object sender, EventArgs e)
+        {
+            tbText.Cut();
+        }
+
+        private void mbEditCopy_Click(object sender, EventArgs e)
+        {
+            tbText.Copy();
+        }
+
+        private void mbEditPaste_Click(object sender, EventArgs e)
+        {
+            tbText.Paste();
+        }
+
+        private void mbEditUndo_Click(object sender, EventArgs e)
+        {
+            tbText.Undo();
+        }
+
+        private void mbEditSelectAll_Click(object sender, EventArgs e)
+        {
+            tbText.SelectAll();
+        }
+
+        private void mbFormatFont_Click(object sender, EventArgs e)
+        {
+            FontDialog fd = new FontDialog();
+            if (fd.ShowDialog() == DialogResult.OK)
+                tbText.Font = fd.Font;
+        }
+
+        private void mbFormatColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+            if (cd.ShowDialog() == DialogResult.OK) ;
+            tbText.ForeColor = cd.Color;
+        }
+
+        private void mbHelpAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Aceasta aplicatie este realizata de catre studentul Golovatic Dan grupa TI-151","Help",MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+
+        private void mbFormatBack_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+            if (cd.ShowDialog() == DialogResult.OK) ;
+            tbText.BackColor = cd.Color;
+        }
+
+        private void mbFileExit_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void mbEditClear_Click(object sender, EventArgs e)
+        {
+            tbText.Clear();
+        }
+
+        private void tbText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
